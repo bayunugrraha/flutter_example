6e3a7766487edee90461c5e131689a920eee58eb
Bayu <bayungraha@gmail.com>
2020-11-14 13:31:12 +0700

    text style

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,29 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  List<Widget> widgets = [];
-  int counter = 1;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan Listview"),
+          title: Text("Latihan Text Style"),
         ),
-        body: ListView(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                RaisedButton(
-                  child: Text("Tambah Data"),
-                  onPressed: () {
-                    setState(() {
-                      widgets.add(Text(
-                        "Data ke" + counter.toString(),
-                        style: TextStyle(fontSize: 35),
-                      ));
-                      counter++;
-                    });
-                  },
-                ),
-                RaisedButton(
-                  child: Text("Hapus Data"),
-                  onPressed: () {
-                    setState(() {
-                      widgets.removeLast();
-                      counter--;
-                    });
-                  },
-                )
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: widgets,
-            )
-          ],
+        body: Center(
+          child: Text(
+            "Ini adalah Text",
+            style: TextStyle(
+                fontFamily: "Poppins",
+                fontStyle: FontStyle.italic,
+                fontSize: 30,
+                decoration: TextDecoration.overline,
+                decorationColor: Colors.red,
+                decorationThickness: 5,
+                decorationStyle: TextDecorationStyle.wavy),
+          ),
         ),
       ),
     );

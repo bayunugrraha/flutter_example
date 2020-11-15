05403e3e1b5f482dc7a519f7ca11eb860984f0b1
Bayu <bayungraha@gmail.com>
2020-11-15 19:13:59 +0700

    textfield decoration

--- lib/main.dart
+++ lib/main.dart
@@ -21,9 +21,27 @@ class _MyAppState extends State<MyApp> {
           margin: EdgeInsets.all(20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
+            children: <Widget>[
               TextField(
-                obscureText: true,
+                decoration: InputDecoration(
+                    fillColor: Colors.lightBlue[50],
+                    filled: true,
+                    icon: Icon(Icons.adb),
+                    suffix: Container(
+                      width: 5,
+                      height: 5,
+                      color: Colors.red,
+                    ),
+                    prefixIcon: Icon(Icons.person),
+                    prefixText: "Name: ",
+                    prefixStyle: TextStyle(
+                        color: Colors.blue, fontWeight: FontWeight.w600),
+                    labelText: "Nama Lengkap",
+                    labelStyle: TextStyle(color: Colors.red),
+                    hintText: "Nama lengkapnya lho...",
+                    hintStyle: TextStyle(fontSize: 12),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
                 onChanged: (value) {
                   setState(() {});
                 },

f7b3b63321d9d7a91e06b2988e1cc65ea329d364
Bayu <bayungraha@gmail.com>
2020-11-19 18:42:41 +0700

    font features

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,64 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:qrscan/qrscan.dart' as scanner;
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() {
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  String text = 'Hasil QR Scan';
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("QR Scan"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(text),
-            SizedBox(
-              height: 20,
-            ),
-            RaisedButton(
-                child: Text("Scan"),
-                onPressed: () async {
-                  text = await scanner.scan();
-                  setState(() {});
-                })
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Typography"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "Contoh 01 (Tanpa apapun)",
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                "Contoh 02 (Small Caps)",
+                style: TextStyle(
+                    fontSize: 20, fontFeatures: [FontFeature.enable('smcp')]),
+              ),
+              Text(
+                "Contoh 03 1/2 (Small Caps & frac)",
+                style: TextStyle(fontSize: 20, fontFeatures: [
+                  FontFeature.enable('smcp'),
+                  FontFeature.enable('frac')
+                ]),
+              ),
+              Text(
+                "Contoh 09 (Poppins)",
+                style: TextStyle(
+                    fontSize: 20, fontFamily: "Poppins", fontFeatures: []),
+              ),
+              Text(
+                "Contoh 09 (Poppins old)",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: "Poppins",
+                    fontFeatures: [FontFeature.oldstyleFigures()]),
+              ),
+              Text(
+                "Contoh poppins (default)",
+                style: TextStyle(fontSize: 20, fontFamily: "Poppins"),
+              ),
+              Text(
+                "Contoh poppins (style set)",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: "Poppins",
+                    fontFeatures: [FontFeature.stylisticSet(5)]),
+              ),
+            ],
+          ),
         ),
       ),
     );

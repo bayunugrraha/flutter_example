3a6d85b30b44cb62aff3862679da0e154e239fbc
Bayu <bayungraha@gmail.com>
2020-11-21 13:57:38 +0700

    switch & animatedswitcher widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_example/user_model.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,30 +8,74 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String output = "no data";
+  bool isON = false;
+  Widget myWidget = Container(
+    width: 200,
+    height: 100,
+    decoration: BoxDecoration(
+        color: Colors.red, border: Border.all(color: Colors.black, width: 3)),
+  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("API Demo"),
+          title: Text("Animated Switcher"),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text(output),
-              RaisedButton(
-                onPressed: () {
-                  User.getUsers("3").then((users) {
-                    output = "";
-                    for (int i = 0; i < users.length; i++)
-                      output = output + "[ " + users[i].name + " ]";
-                    setState(() {});
+              AnimatedSwitcher(
+                child: myWidget,
+                duration: Duration(seconds: 1),
+                transitionBuilder: (child, animation) => RotationTransition(
+                  turns: animation,
+                  child: child,
+                ),
+              ),
+              Switch(
+                activeColor: Colors.green,
+                inactiveThumbColor: Colors.red,
+                inactiveTrackColor: Colors.red[200],
+                value: isON,
+                onChanged: (newValue) {
+                  isON = newValue;
+                  setState(() {
+                    if (isON)
+                      myWidget = Container(
+                        key: ValueKey(1),
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            border: Border.all(color: Colors.black, width: 3)),
+                      );
+                    // myWidget = SizedBox(
+                    //   width: 200,
+                    //   height: 100,
+                    //   child: Center(
+                    //     child: Text(
+                    //       "Switch: ON",
+                    //       style: TextStyle(
+                    //           color: Colors.green,
+                    //           fontWeight: FontWeight.w700,
+                    //           fontSize: 20),
+                    //     ),
+                    //   ),
+                    // );
+                    else
+                      myWidget = Container(
+                        key: ValueKey(2),
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            border: Border.all(color: Colors.black, width: 3)),
+                      );
                   });
                 },
-                child: Text("GET"),
               )
             ],
           ),

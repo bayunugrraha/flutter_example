6778b887d6260fafe543960d8fbaaf3a53c26d4f
Bayu <bayungraha@gmail.com>
2020-11-26 15:52:05 +0700

    product card (boxshadow, roundedrectangleborder)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_example/product_card.dart';
 import 'package:provider/provider.dart';
-import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -8,102 +8,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Custom Progress Bar"),
-          ),
-          body: Center(
-            child: ChangeNotifierProvider<TimeState>(
-              create: (context) => TimeState(),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Consumer(
-                      builder: (context, timeState, _) => CustomProgressBar(
-                          width: 200, value: timeState.time, totalValue: 15)),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Consumer<TimeState>(
-                    builder: (context, timeState, _) => RaisedButton(
-                        color: Colors.lightBlue,
-                        child: Text(
-                          "Start",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        onPressed: () {
-                          Timer.periodic(Duration(seconds: 1), (timer) {
-                            if (timeState.time == 0)
-                              timer.cancel();
-                            else
-                              timeState.time -= 1;
-                          });
-                        }),
-                  )
-                ],
-              ),
-            ),
-          )),
-    );
-  }
-}
-
-class CustomProgressBar extends StatelessWidget {
-  final double width;
-  final int value;
-  final int totalValue;
-
-  CustomProgressBar({this.width, this.value, this.totalValue});
-
-  @override
-  Widget build(BuildContext context) {
-    double ratio = value / totalValue;
-
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(Icons.timer, color: Colors.grey[700]),
-        SizedBox(
-          width: 5,
+        appBar: AppBar(
+          backgroundColor: firstColor,
         ),
-        Stack(
-          children: [
-            Container(
-              width: width,
-              height: 10,
-              decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(5)),
-            ),
-            Material(
-              borderRadius: BorderRadius.circular(5),
-              child: AnimatedContainer(
-                height: 10,
-                width: width * ratio,
-                duration: Duration(milliseconds: 500),
-                decoration: BoxDecoration(
-                    color: (ratio < 0.3)
-                        ? Colors.red
-                        : (ratio < 0.6)
-                            ? Colors.amber[400]
-                            : Colors.lightGreen,
-                    borderRadius: BorderRadius.circular(5)),
-              ),
-            )
-          ],
-        )
-      ],
+        body: ChangeNotifierProvider<ProductState>(
+          create: (context) => ProductState(),
+          child: Container(
+            margin: EdgeInsets.all(20),
+            child: Align(
+                alignment: Alignment.topCenter,
+                child: Consumer<ProductState>(
+                  builder: (context, productState, _) => ProductCard(
+                    imageURL:
+                        "https://lelogama.go-jek.com/cms_editor/2018/09/21/kuliner_asyik_bandung03.jpeg",
+                    name: "Chesee Pizza Reguler",
+                    price: "Rp15.000",
+                    quantity: productState.quantity,
+                    notification:
+                        (productState.quantity > 5) ? "Diskon 10%" : null,
+                    onAddCartTap: () {},
+                    onIncTap: () {
+                      productState.quantity++;
+                    },
+                    onDecTap: () {
+                      productState.quantity--;
+                    },
+                  ),
+                )),
+          ),
+        ),
+      ),
     );
   }
 }
 
-class TimeState with ChangeNotifier {
-  int _time = 15;
+class ProductState with ChangeNotifier {
+  int _quantity = 0;
 
-  int get time => _time;
-  set time(int newTime) {
-    _time = newTime;
+  int get quantity => _quantity;
+  set quantity(int newValue) {
+    _quantity = newValue;
     notifyListeners();
   }
 }

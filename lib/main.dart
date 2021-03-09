c44f636f2503331b613bf692969cbef0c1c9e106
Bayu <bayungraha@gmail.com>
2021-03-09 21:15:42 +0700

    SelectableText, ToggleButtons, ColorFiltered

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_example/product_card.dart';
-import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<bool> isSelected = [true, false, false];
+  ColorFilter colorFilter = ColorFilter.mode(Colors.blue, BlendMode.screen);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+        home: ColorFiltered(
+      colorFilter: colorFilter,
+      child: Scaffold(
         appBar: AppBar(
-          backgroundColor: firstColor,
+          title: Text("Widgets Demo GDG 2019 China"),
         ),
-        body: ChangeNotifierProvider<ProductState>(
-          create: (context) => ProductState(),
-          child: Container(
-            margin: EdgeInsets.all(20),
-            child: Align(
-                alignment: Alignment.topCenter,
-                child: Consumer<ProductState>(
-                  builder: (context, productState, _) => ProductCard(
-                    imageURL:
-                        "https://lelogama.go-jek.com/cms_editor/2018/09/21/kuliner_asyik_bandung03.jpeg",
-                    name: "Chesee Pizza Reguler",
-                    price: "Rp15.000",
-                    quantity: productState.quantity,
-                    notification:
-                        (productState.quantity > 5) ? "Diskon 10%" : null,
-                    onAddCartTap: () {},
-                    onIncTap: () {
-                      productState.quantity++;
-                    },
-                    onDecTap: () {
-                      productState.quantity--;
-                    },
-                  ),
-                )),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SelectableText(
+                "Ini adalah Selectable text. Silahkan pilih saya.",
+                style: TextStyle(fontSize: 20),
+                showCursor: true,
+                cursorWidth: 3,
+                cursorColor: Colors.red,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ToggleButtons(
+                isSelected: isSelected,
+                children: [
+                  Icon(Icons.access_alarm),
+                  Icon(Icons.adb),
+                  Icon(Icons.add_comment)
+                ],
+                onPressed: (index) {
+                  setState(() {
+                    if (index == 0)
+                      colorFilter =
+                          ColorFilter.mode(Colors.blue, BlendMode.screen);
+                    else if (index == 1)
+                      colorFilter =
+                          ColorFilter.mode(Colors.green, BlendMode.softLight);
+                    else
+                      colorFilter =
+                          ColorFilter.mode(Colors.purple, BlendMode.multiply);
+                    for (int i = 0; i < isSelected.length; i++)
+                      isSelected[i] = (i == index) ? true : false;
+                  });
+                },
+                fillColor: Colors.red[50],
+                selectedColor: Colors.red,
+                selectedBorderColor: Colors.red,
+                splashColor: Colors.blue,
+                highlightColor: Colors.yellow,
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class ProductState with ChangeNotifier {
-  int _quantity = 0;
-
-  int get quantity => _quantity;
-  set quantity(int newValue) {
-    _quantity = newValue;
-    notifyListeners();
+    ));
   }
 }

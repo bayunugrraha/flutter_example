6656bfd0483d19791c2a4b97df1d10ea7951e588
Bayu <bayungraha@gmail.com>
2021-03-16 17:24:58 +0700

    MVVM Architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_example/bloc/user_bloc.dart';
+import 'package:flutter_example/ui/main_page.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  List<bool> isSelected = [true, false, false];
-  ColorFilter colorFilter = ColorFilter.mode(Colors.blue, BlendMode.screen);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: ColorFiltered(
-      colorFilter: colorFilter,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Widgets Demo GDG 2019 China"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SelectableText(
-                "Ini adalah Selectable text. Silahkan pilih saya.",
-                style: TextStyle(fontSize: 20),
-                showCursor: true,
-                cursorWidth: 3,
-                cursorColor: Colors.red,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              ToggleButtons(
-                isSelected: isSelected,
-                children: [
-                  Icon(Icons.access_alarm),
-                  Icon(Icons.adb),
-                  Icon(Icons.add_comment)
-                ],
-                onPressed: (index) {
-                  setState(() {
-                    if (index == 0)
-                      colorFilter =
-                          ColorFilter.mode(Colors.blue, BlendMode.screen);
-                    else if (index == 1)
-                      colorFilter =
-                          ColorFilter.mode(Colors.green, BlendMode.softLight);
-                    else
-                      colorFilter =
-                          ColorFilter.mode(Colors.purple, BlendMode.multiply);
-                    for (int i = 0; i < isSelected.length; i++)
-                      isSelected[i] = (i == index) ? true : false;
-                  });
-                },
-                fillColor: Colors.red[50],
-                selectedColor: Colors.red,
-                selectedBorderColor: Colors.red,
-                splashColor: Colors.blue,
-                highlightColor: Colors.yellow,
-                borderRadius: BorderRadius.circular(15),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ));
+        debugShowCheckedModeBanner: false,
+        home: BlocProvider(
+          builder: (context) => UserBloc(),
+          child: MainPage(),
+        ));
   }
 }
